9e483db3a5182292dce7ba8149c42a32e83a3cb5
long.gf <long.gf@topsports.com.cn>
2025-04-28 15:19:26 +0800

    init

---
+++ spring-ai-examples/mcp-weather-stdio-server/src/main/java/com/lgfei/ai/example/McpWeatherStdioServerApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.ai.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpWeatherStdioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpWeatherStdioServerApplication.class, args);
+	}
+
+}
---
+++ spring-ai-examples/mcp-weather-stdio-server/src/test/java/com/lgfei/ai/example/McpWeatherStdioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.ai.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpWeatherStdioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
