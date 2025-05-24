5b8cdfbb3f90a69352f68a1bee4436111b8d3421
long.gf <long.gf@topsports.com.cn>
2025-05-24 15:25:33 +0800

    init chat-client

---
+++ spring-ai-examples/chat-client/src/test/java/com/lgfei/ai/example/chatclient/ChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.ai.example.chatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
