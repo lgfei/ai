4776f903ff6e1a630a6de93dd3213d986abe63a2
long.gf <long.gf@topsports.com.cn>
2025-06-21 16:43:00 +0800

    init goashore

---
+++ goashore/src/main/java/com/lgfei/ai/goashore/GoashoreApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.ai.goashore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoashoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoashoreApplication.class, args);
+	}
+
+}
